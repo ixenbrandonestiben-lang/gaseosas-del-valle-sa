f7f38b6637cd6e63e9365a0721c676a5a31049ef
shen <ixenbrandonestiben@gmail.com>
2026-08-13 07:51:06 -0600

    :sparkles: feat(dml) insercion de datos a la tabla de productos

---
+++ dml/inserciones.sql
@@ -0,0 +1,66 @@
+use gaseosas_del_valle;
+
+
+INSERT INTO productos
+(id_producto, nombre_producto, categoria_producto, precio_producto,
+ volumen_ml, estado_producto, usuario_registro)
+VALUES
+(1,'Cola Clásica','Cola',5.00,355,'activo','ADMIN'),
+(2,'Cola Original','Cola',6.00,500,'activo','ADMIN'),
+(3,'Cola Tradicional','Cola',7.00,600,'activo','ADMIN'),
+(4,'Cola Familiar','Cola',12.00,1000,'activo','ADMIN'),
+(5,'Cola 500ml','Cola',18.00,1500,'activo','ADMIN'),
+
+(6,'Cola Zero','Cola Zero',7.00,2500,'activo','ADMIN'),
+(7,'Cola Zero Familiar','Cola Zero',15.00,355,'activo','ADMIN'),
+(8,'Cola Zero 600ml','Cola Zero',8.00,500,'activo','ADMIN'),
+(9,'Cola Zero 1.5L','Cola Zero',20.00,600,'activo','ADMIN'),
+(10,'Cola Zero 2.5L','Cola Zero',30.00,1500,'activo','ADMIN'),
+
+(11,'Limonada Natural','Limonada',5.00,2500,'activo','ADMIN'),
+(12,'Limonada Familiar','Limonada',14.00,355,'activo','ADMIN'),
+(13,'Limonada 600ml','Limonada',8.00,500,'activo','ADMIN'),
+(14,'Limonada 1.5L','Limonada',20.00,600,'activo','ADMIN'),
+(15,'Limonada Light','Limonada',9.00,1500,'activo','ADMIN'),
+
+(16,'Naranja Clásica','Naranja',5.00,2500,'activo','ADMIN'),
+(17,'Naranja Familiar','Naranja',14.00,355,'activo','ADMIN'),
+(18,'Naranja 600ml','Naranja',8.00,500,'activo','ADMIN'),
+(19,'Naranja 1.5L','Naranja',20.00,600,'activo','ADMIN'),
+(20,'Naranja Zero','Naranja',22.00,1500,'activo','ADMIN'),
+
+(21,'Uva Clásica','Uva',5.00,2500,'activo','ADMIN'),
+(22,'Uva Familiar','Uva',14.00,355,'activo','ADMIN'),
+(23,'Uva 600ml','Uva',8.00,500,'activo','ADMIN'),
+(24,'Uva 1.5L','Uva',20.00,600,'activo','ADMIN'),
+(25,'Uva Light','Uva',22.00,1500,'activo','ADMIN'),
+
+(26,'Manzana Clásica','Manzana',5.00,2500,'activo','ADMIN'),
+(27,'Manzana Familiar','Manzana',14.00,355,'activo','ADMIN'),
+(28,'Manzana 600ml','Manzana',8.00,500,'activo','ADMIN'),
+(29,'Manzana 1.5L','Manzana',20.00,600,'activo','ADMIN'),
+(30,'Manzana Light','Manzana',22.00,1500,'activo','ADMIN'),
+
+(31,'Té Limón','Té',10.00,2500,'activo','ADMIN'),
+(32,'Té Durazno','Té',10.00,355,'activo','ADMIN'),
+(33,'Té Verde','Té',10.00,500,'activo','ADMIN'),
+(34,'Té Frutos Rojos','Té',11.00,500,'activo','ADMIN'),
+(35,'Té Melocotón','Té',10.00,500,'activo','ADMIN'),
+
+(36,'Agua Pura 500ml','Agua',3.00,500,'activo','ADMIN'),
+(37,'Agua Pura 1L','Agua',5.00,1000,'activo','ADMIN'),
+(38,'Agua Pura 1.5L','Agua',7.00,1500,'activo','ADMIN'),
+(39,'Agua Pura 2.5L','Agua',12.00,2500,'activo','ADMIN'),
+(40,'Agua Pura 5L','Agua',20.00,5000,'activo','ADMIN'),
+
+(41,'Energizante Original','Energizante',12.00,355,'activo','ADMIN'),
+(42,'Energizante Tropical','Energizante',12.00,355,'activo','ADMIN'),
+(43,'Energizante Zero','Energizante',12.00,355,'activo','ADMIN'),
+(44,'Energizante Citrus','Energizante',12.00,355,'activo','ADMIN'),
+(45,'Energizante Frutos Rojos','Energizante',12.00,355,'activo','ADMIN'),
+
+(46,'Frutal Mango','Frutal',9.00,355,'activo','ADMIN'),
+(47,'Frutal Piña','Frutal',9.00,500,'activo','ADMIN'),
+(48,'Frutal Fresa','Frutal',9.00,500,'activo','ADMIN'),
+(49,'Frutal Tropical','Frutal',9.00,500,'activo','ADMIN'),
+(50,'Frutal Cítrico','Frutal',9.00,500,'activo','ADMIN');
